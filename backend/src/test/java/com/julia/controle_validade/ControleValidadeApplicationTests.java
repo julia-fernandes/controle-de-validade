a579fc9287677fe4df6132a81548f6f39b7d96b5
Julia <juliafcostads@gmail.com>
2026-09-25 17:41:13 -0300

    Initial commit

---
+++ backend/src/test/java/com/julia/controle_validade/ControleValidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julia.controle_validade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleValidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
